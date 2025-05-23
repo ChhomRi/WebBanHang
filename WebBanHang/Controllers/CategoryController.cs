2a362ab665e9f5543f163d2f49ff33f02d1a1b3b
ADMIN <ADMIN@PC345>
2025-05-23 11:22:54 +0700

    lab 03

---
+++ WebBanHang/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+
+        public IActionResult Index()
+        {
+            var listCategory = _db.Categories.ToList();
+            return View(listCategory);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Thêm danh mục thành công";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Cập nhật danh mục thành công";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        // Xử lý xóa chủng loại
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted success";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,17 +14,26 @@ namespace WebBanHang.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var pageSize = 3;
+            var dsSanPham = _db.Products.Include(x => x.Category).ToList();
+            return View(dsSanPham.Skip(0).Take(pageSize).ToList());
         }
-
+        public IActionResult LoadMore(int page = 2)
+        {
+            var pageSize = 3;
+            var dsSanPham = _db.Products.Include(x => x.Category).ToList();
+            return PartialView("_ProductPartial", dsSanPham.Skip((page - 1) * pageSize).Take(pageSize).ToList());
+        }
         public IActionResult Privacy()
         {
             return View();
