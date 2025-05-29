7a3fe8b214623007de4fa792043ded02d76cdb34
Chhom Ri <chhomri02@gmail.com>
2025-05-29 09:33:19 +0700

    DK

--- WebBanHang/Startup.cs
+++ WebBanHang/Startup.cs
@@ -60,7 +60,7 @@ namespace WebBanHang
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            
             app.UseAuthentication();
             app.UseAuthorization();
 
