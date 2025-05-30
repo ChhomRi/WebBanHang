9eb1978507397a51c1c4b4788da17cb8656ce4fd
ADMIN <ADMIN@PC345>
2025-05-30 10:55:26 +0700

    admin

--- WebBanHang/Startup.cs
+++ WebBanHang/Startup.cs
@@ -67,6 +67,10 @@ namespace WebBanHang
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                   name: "area",
+                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
